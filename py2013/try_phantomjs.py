19d032e97874db42036173c245b1ebf6097614df
Jay <smile665@gmail.com>
2014-05-25 21:45:09 +0800

    try_phantomjs.py: add find_element_by_xpath

--- py2013/try_phantomjs.py
+++ py2013/try_phantomjs.py
@@ -6,6 +6,7 @@ Created on Dec 6, 2013
 
 @author: Jay <smile665@gmail.com>
 @description: use PhantomJS to parse a web page to get the geo info of an IP
+              For PhantomJS, please visit: http://phantomjs.org/
 '''
 
 from selenium import webdriver
@@ -19,4 +20,7 @@ driver.get("http://www.ip.cn/125.95.26.81")
 #print driver.current_url
 #print driver.page_source
 print driver.find_element_by_id('result').text.split('\n')[0].split('来自：')[1]
+#text =  driver.find_element_by_xpath('//div[@id="result"]/div/p').text
+#result = text.split('来自：')[1]
+#print result
 driver.quit
