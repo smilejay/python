9b12f86c8813541105be387e0e31bcc561667c94
Jay <smile665@gmail.com>
2015-08-28 13:17:37 +0800

    get first/last day of a month

--- py2013/date_test.py
+++ py2013/date_test.py
@@ -9,13 +9,18 @@ Created on Sep 20, 2013
 import datetime
 import calendar
 
+
 def get_a_month_val(year, month, day=1):
     day = datetime.datetime(year, month, day)
     print day
-    last_day_of_previous_month = day.replace(day=1) - datetime.timedelta(days=1)
-    last_day_of_this_month = day.replace(day=calendar.monthrange(year, month)[1])
-    print last_day_of_previous_month
-    print last_day_of_this_month
+    last_day_previous_month = day.replace(day=1) - datetime.timedelta(days=1)
+    first_day_this_month = day.replace(day=1)
+    last_day_this_month = day.replace(day=calendar.monthrange(year, month)[1])
+    last_day_this_month_1 = day.replace(day=calendar.mdays[day.month])
+    print last_day_previous_month
+    print first_day_this_month
+    print last_day_this_month
+    print last_day_this_month_1
 
 if __name__ == '__main__':
     get_a_month_val(2013, 10, 20)
