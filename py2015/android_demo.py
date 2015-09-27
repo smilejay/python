75978b1356c2fb786c92b3f9d078876944d5cfed
Jay <smile665@gmail.com>
2015-09-27 10:42:21 +0800

    adjust code format of android_demo.py

--- py2015/android_demo.py
+++ py2015/android_demo.py
@@ -1,4 +1,4 @@
-#/usr/bin/env python
+#!/usr/bin/env python
 # -*- coding:utf-8 -*-
 '''
 Create on 2015-05-05
@@ -8,13 +8,14 @@ import unittest
 from selenium import webdriver
 import time
 
-#Appium环境配置
+# Appium环境配置
 PATH = lambda p: os.path.abspath(
     os.path.join(os.path.dirname(__file__), p)
 )
 
 
 class DpAppTests(unittest.TestCase):
+
     def setUp(self):
         desired_caps = {}
         desired_caps['platformName'] = 'Android'
@@ -26,16 +27,19 @@ class DpAppTests(unittest.TestCase):
             'apps/Nova_7.2.0_debug.apk'
         )
         desired_caps['appPackage'] = 'com.dianping.v1'
-        desired_caps['appActivity'] = 'com.dianping.main.guide.SplashScreenActivity'
+        desired_caps[
+            'appActivity'] = 'com.dianping.main.guide.SplashScreenActivity'
 
-        self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+        self.driver = webdriver.Remote(
+            'http://localhost:4723/wd/hub', desired_caps)
 
     def tearDown(self):
         self.driver.quit()
 
     def test_dpApp(self):
         time.sleep(10)
-        el = self.driver.find_element_by_xpath("//android.widget.TextView[contains(@text,'美食')]")
+        el = self.driver.find_element_by_xpath(
+            "//android.widget.TextView[contains(@text,'美食')]")
         el.click()
 
 
