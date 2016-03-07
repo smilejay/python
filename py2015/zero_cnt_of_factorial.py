25c4bb80aacfad9b6827e987a1470d263655ca42
Vivek <vivekvsingh14@gmail.com>
2016-03-07 23:45:39 +0530

    Update zero_cnt_of_factorial.py
    Made the function short

--- py2015/zero_cnt_of_factorial.py
+++ py2015/zero_cnt_of_factorial.py
@@ -5,16 +5,10 @@ calculate how many zero(0)s in the end of n!.
 
 def zero_cnt_of_factorial(num=50):
     count = 0
-    delta = 5
-    m = 5
-    while m <= num:
-        count += 1
-        m1 = m / delta
-        while m1 > 0:
-            if m1 % delta == 0:
-                count += 1
-            m1 = m1 / delta
-        m += 5
+    i=5
+    while ( (num/i)>=1):
+        count += num/i
+        i *= 5
     return count
 
 if __name__ == '__main__':
