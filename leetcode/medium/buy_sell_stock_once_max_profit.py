8b255c1cc7822c48e782d4e06445f52e31366148
Jay <smile665@gamil.com>
2022-04-27 08:46:44 +0800

    leetcode: add buy_sell_stock_once_max_profit.py

---
+++ leetcode/medium/buy_sell_stock_once_max_profit.py
@@ -0,0 +1,34 @@
+# -*- coding:UTF-8 -*-
+'''
+给定一个数组 prices ，它的第 i 个元素 prices[i] 表示一支给定股票第 i 天的价格。
+你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票。设计一个算法来计算你所能获取的最大利润。
+返回你可以从这笔交易中获取的最大利润。如果你不能获取任何利润，返回 0 。
+
+思路：
+如果我是在历史最低点买的股票就好了！太好了，在题目中，我们只要用一个变量记录一个历史最低价格 minprice，
+我们就可以假设自己的股票是在那天买的。那么我们在第 i 天卖出股票能得到的利润就是 prices[i] - minprice。
+因此，我们只需要遍历价格数组一遍，记录历史最低点，然后在每一天考虑这么一个问题：
+如果我是在历史最低点买进的，那么我今天卖出能赚多少钱？当考虑完所有天数之时，我们就得到了最好的答案。
+
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock
+'''
+
+
+class Solution:
+    def maxProfit(self, prices):
+        min_price = float('inf')
+        max_profit = 0
+        for price in prices:
+            min_price = min(min_price, price)
+            max_profit = max(max_profit, price - min_price)
+        return max_profit
+
+
+if __name__ == '__main__':
+    l1 = [7,1,5,3,6,4]
+    l2 = [7,6,4,3,1]
+    s = Solution()
+    print(s.maxProfit(l1))
+    print(s.maxProfit(l2))
